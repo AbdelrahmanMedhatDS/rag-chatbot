5a29d281d1b76c04604487308f03b846e8245346
Abdelrahman Medhat <a.medhatahmed9@gmail.com>
2026-01-30 02:27:48 +0200

    Read an uploaded file from the client and save it to disk without loading waiting till the whole file is being ready into memory

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,10 @@
+import os
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers import get_settings, Settings
-from controllers import DataController
-
+from controllers import DataController, ProjectController
+from models import ResponseSignal
+import aiofiles # async file handling lib
 data_router = APIRouter(
     prefix="/api/v1/data",
     tags=["api_v1" , "data"],
@@ -25,4 +27,19 @@ async def upload_data(project_id:str, file:UploadFile,
         )
     
     # handle file storage step1:
+    project_dir_path  = ProjectController().get_project_path(project_id=project_id)
+    file_path = os.path.join(
+        project_dir_path,
+        file.filename
+    )
     
+    async with aiofiles.open(file_path, "wb") as f:
+        while chunk := await file.read(512000) :
+            await f.write(chunk)
+
+    return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            }
+        )
