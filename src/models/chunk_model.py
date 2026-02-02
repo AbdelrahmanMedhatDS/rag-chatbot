e8d56bc3ac57f07d4f40509f5e7b4e6293da3ce3
Abdelrahman Medhat <a.medhatahmed9@gmail.com>
2026-02-02 10:25:33 +0200

    feat: implement NLP routes and schemas for project indexing and searching

--- src/models/chunk_model.py
+++ src/models/chunk_model.py
@@ -68,6 +68,18 @@ class ChunkModel(BaseDataModel):
 
         return result.deleted_count
     
+
+    async def get_poject_chunks(self, project_id: ObjectId, page_no: int=1, page_size: int=50):
+        records = await self.db_collection.find({
+                    "chunk_project_id": project_id
+                }).skip(
+                    (page_no-1) * page_size
+                ).limit(page_size).to_list(length=None)
+
+        return [
+            ChunkSchema(**record)
+            for record in records
+        ]
     
 
     
--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -2,3 +2,4 @@ from .requests.data_schema import ProcessRequest
 from .database.chunk_shema import  ChunkSchema
 from .database.project_shema import ProjectSchema
 from .database.asset_shema import AssetSchema
+from .requests.nlp_schema import PushRequest, SearchRequest
